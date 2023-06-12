9811ffd20fe36bf85a6d2fd23e3cd042f6c9089f
Amirul Zharfan Zalid <am.zharfan@gmail.com>
2023-06-12 08:40:42 +0800

    Move file to fix git casing issue

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import DashNavbar from "../../components/dashboard/dashnavbar";
+import DashNavbar from "../../components/dashnavbar";
 
 export default function Profile() {
     const { data: session, status } = useSession({
