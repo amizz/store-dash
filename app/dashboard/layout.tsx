092df7718a59ea39608c15d4541c0c7bb58ae930
Amirul Zharfan Zalid <am.zharfan@gmail.com>
2023-06-17 00:10:25 +0800

    Get companies fetching

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,15 +2,22 @@
 
 import { useSession } from "next-auth/react";
 import DashNavbar from "../components/dashboard/dashnavbar";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import SubsNotice from "../components/dashboard/subsnotice";
 import Script from "next/script";
+import { useEffect, useState } from "react";
+import { useCompanyStore } from "../store/company";
+import { Companies } from "../api/companies/route";
 
 export default function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const pathname = usePathname();
+    const [companies, setCompanies] = useState<Companies>([]);
+    const [isLoading, setLoading] = useState(false);
+    const { currentCompany, setDefaultCompany } = useCompanyStore();
     const { data: session, status } = useSession({
         required: true,
         onUnauthenticated() {
@@ -18,15 +25,34 @@ export default function DashboardLayout({
         },
     });
 
-    if (status === "loading") {
+    useEffect(() => {
+        setLoading(true);
+        fetch("/api/companies")
+            .then((res) => res.json())
+            .then((data) => {
+                setCompanies(data);
+                setDefaultCompany(data);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (status === "loading" || isLoading) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
             <Script src="https://cdn.jsdelivr.net/npm/@preline/dropdown@1.3.0/index.min.js"></Script>
-            <DashNavbar name={session?.user?.name ?? ""}></DashNavbar>
-            <SubsNotice></SubsNotice>
+            <DashNavbar
+                name={session?.user?.name ?? ""}
+                companies={companies}
+            ></DashNavbar>
+            {!currentCompany?.active && (
+                <div>
+                    <SubsNotice></SubsNotice>
+                    <div className="z-50 w-full h-full absolute"></div>
+                </div>
+            )}
             <section>{children}</section>
         </div>
     );
