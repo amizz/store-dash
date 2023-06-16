5962c500007d57a557a82b81838ffea008ae6e48
Amirul Zharfan Zalid <am.zharfan@gmail.com>
2023-06-16 20:48:54 +0800

    Subscription notice warning

---
+++ app/components/dashboard/subsnotice.tsx
@@ -0,0 +1,17 @@
+export default function SubsNotice() {
+    return (
+        <section className="w-full max-w-5xl mx-auto p-1 py-5">
+            <div className="flex flex-col bg-white border shadow-sm rounded-xl   ">
+                <div className="p-4 md:p-7">
+                    <h3 className="text-lg font-bold text-gray-800 ">
+                        Subscription Notice
+                    </h3>
+                    <p className="mt-2 text-gray-800 ">
+                        You did not have any active subscription. Select or
+                        create a new company, and subscribe to a plan.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
